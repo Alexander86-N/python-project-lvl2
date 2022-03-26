08b008cc8407f4a4e000867b3f8195febef2843e
Aleksandr <nastyavin86@br.ru>
2022-03-26 18:09:17 +0300

    change gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,6 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f FORMAT', '--format FORMAT', dest='', help='set format of output')
+
 
 def print_h():
     return parser.print_help()
