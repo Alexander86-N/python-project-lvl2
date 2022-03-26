d7c1ecb35ca106775acb8258b6cc4a6f25fae3e7
Aleksandr <nastyavin86@br.ru>
2022-03-26 15:02:57 +0300

    changed gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,4 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
-parser.print_help()
+
+
+if __name__ == '__main__':
+    parser.print_help()
