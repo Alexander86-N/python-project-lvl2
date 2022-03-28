ab2de41b5706a51eb3956b4bcb294e1885de3964
Aleksandr <nastyavin86@br.ru>
2022-03-28 21:06:01 +0300

    change test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,24 +1,15 @@
 from gendiff import generate_diff
 
-
-file1.json = {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": false
-        }
-file2.json = {
-        "timeout": 20,
-        "verbose": true,
-        "host": "hexlet.io"
-        }
-result = {
-        - follow: false
-          host: hexlet.io
-        - proxy: 123.234.53.22
-        - timeout: 50
-        + timeout: 20
-        + verbose: true
-        }
+test_file1 = "/home/golem86/example/one/two/file1.json"
+test_file2 = "/home/golem86/example/file2.json"
+result =
+{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}
 def test_generate_diff():
-    assert generate_diff(file1.json, file2.json) == result
+    assert generate_diff(test_file1, test_file2) == result
