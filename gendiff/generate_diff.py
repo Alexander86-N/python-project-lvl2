ad1ccf1b1d503e560ad79a978f0672b44b39e657
Aleksandr <nastyavin86@br.ru>
2022-03-29 18:32:57 +0300

    change gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -11,7 +11,8 @@ def generate_diff(file_path1, file_path2):
 def element_research(file_one, file_two):
     result = ['{']
     res = []
-    list_one, list_two = [_ for _ in file_one.items()], [_ for _ in file_two.items()]
+    list_one = [_ for _ in file_one.items()]
+    list_two = [_ for _ in file_two.items()]
     res.extend(list_one)
     res.extend([_ for _ in list_two if _ not in list_one])
     for item in sorted(res, key=lambda item: item[0]):
@@ -27,7 +28,8 @@ def element_research(file_one, file_two):
 
 def convert_to_string(items, symbol, indent=' '):
     item1, item2 = items
-    key = symbol + indent + item1.rstrip('"') if isinstance(item1, str) else f'{symbol}{indent}{item1}'
-    value = item2.rstrip('"') if isinstance(item2, str) else\
-            str(item2).lower() if isinstance(item2, bool) else item2
+    key = symbol + indent + item1.rstrip('"') if isinstance(item1, str)\
+        else f'{symbol}{indent}{item1}'
+    value = item2.rstrip('"') if isinstance(item2, str)\
+        else str(item2).lower() if isinstance(item2, bool) else item2
     return f'{indent * 2}{key}: {value}'
