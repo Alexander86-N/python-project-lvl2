c79fcbefd5abdb03eb01228b1faedce01fe87596
Aleksandr <nastyavin86@br.ru>
2022-04-02 19:57:20 +0300

    change poetry.lock pyproject.toml tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,23 @@
+import json
+import yaml
 from gendiff import generate_diff
+
 
 test_file1 = "tests/fixtures/file1.json"
 test_file2 = "tests/fixtures/file2.json"
 test_file3 = "tests/fixtures/file3.json"
-result1 = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+test_file4 = "tests/fixtures/file1.yaml"
+test_file5 = "tests/fixtures/file2.yml"
+with open('tests/fixtures/result.txt') as text:
+    result1 = text.read()
 result2 = '{\n    host: hexlet.io\n    timeout: 20\n    verbose: true\n}'
 def test_generate_diff():
-    assert generate_diff.generate_diff(test_file1, test_file2) == result1
+    assert generate_diff.generate_diff(test_file1, test_file2) == result1[:-1]
     assert generate_diff.generate_diff(test_file3, test_file2) == result2
+    assert generate_diff.generate_diff(test_file4, test_file5) == result1[:-1]
+
+
+def test_determine_file_format():
+    assert generate_diff.determine_file_format(test_file1) == json.load(open(test_file1))
+    assert generate_diff.determine_file_format(test_file4) == yaml.safe_load(open(test_file4))
+    assert generate_diff.determine_file_format(test_file5) == yaml.safe_load(open(test_file5))
