650ed9e9ae3e00f7dc40143d56459f7bb5d2a771
Aleksandr <nastyavin86@br.ru>
2022-04-08 19:19:37 +0300

    change gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,6 +5,7 @@ from gendiff.engine import diff
 
 
 def generate_diff(file_path1, file_path2):
+    """ Collects and outputs diff. """
     file1 = determine_file_format(file_path1)
     file2 = determine_file_format(file_path2)
     result = diff.diff(file1, file2)
@@ -12,6 +13,7 @@ def generate_diff(file_path1, file_path2):
 
 
 def determine_file_format(file_path):
+    """ Determines the file format and opens it. """
     if file_path.endswith('.json'):
         return json.load(open(file_path))
     if file_path.endswith('.yml') or file_path.endswith('.yaml'):
