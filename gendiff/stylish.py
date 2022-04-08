770655b554a0d9e147b760a1bd267f600eb92540
Aleksandr <nastyavin86@br.ru>
2022-04-08 19:20:32 +0300

    change gendiff/stylish.py

--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,31 +1,34 @@
-def stylish(diff_list, level=0):
-    result = '{\n'
-    indent = '  '
-    for i in range(level):
+SYMBOLS = {
+        "added": "+ ",
+        "available": "- ",
+        "parent": "  ",
+        "same": "  "
+        }
+
+
+def stylish(diff_list, depth=0, indent='  '):
+    """ Forms a text representation of the list. """
+    result = ['{']
+    for i in range(depth):
         indent += '    '
     for node in diff_list:
         if node['status'] == 'parent':
-            data = stylish(node['children'], level + 1)
-            result += f"{indent}  {node['name']}: {data}\n"
-        if node['status'] == 'same':
-            data = format_data(node['value'], indent)
-            result += f"{indent}  {node['name']}: {data}\n"
-        if node['status'] == 'available':
-            data = format_data(node['value'], indent)
-            result += f"{indent}- {node['name']}: {data}\n"
-        if node['status'] == 'added':
-            data = format_data(node['value'], indent)
-            result += f"{indent}+ {node['name']}: {data}\n"
-        if node['status'] == 'changed':
-            data = format_data(node['value before'], indent)
-            result += f"{indent}- {node['name']}: {data}\n"
-            data = format_data(node['value after'], indent)
-            result += f"{indent}+ {node['name']}: {data}\n"
-    result += indent[:-2] + '}'
-    return result
+            result.append(f"{indent}{SYMBOLS.get(node['status'])}{node['name']}:\
+ {stylish(node['children'], depth + 1)}")
+        elif node['status'] == 'changed':
+            result.append(f"{indent}{SYMBOLS['available']}{node['name']}:\
+ {format_data(node['value before'], indent)}")
+            result.append(f"{indent}{SYMBOLS['added']}{node['name']}:\
+ {format_data(node['value after'], indent)}")
+        else:
+            result.append(f"{indent}{SYMBOLS.get(node['status'])}{node['name']}:\
+ {format_data(node['value'], indent)}")
+    result.append(indent[:-2] + '}')
+    return '\n'.join(result)
 
 
 def format_data(data, indent):
+    """ Modifies each element of the incoming data ."""
     if isinstance(data, dict):
         indent += '    '
         result = '{\n'
@@ -34,5 +37,6 @@ def format_data(data, indent):
             result += f'{indent}  {key}: {value}\n'
         result += indent[:-2] + '}'
     else:
-        result = str(data).lower() if isinstance(data, bool) else 'null' if isinstance(data, type(None)) else str(data)
+        result = str(data).lower() if isinstance(data, bool) else\
+                'null' if isinstance(data, type(None)) else str(data)
     return result
