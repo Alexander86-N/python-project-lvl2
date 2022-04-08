2b523b03200e589a4c484cc2fdd37e2d271dd1fe
Aleksandr <nastyavin86@br.ru>
2022-04-08 21:54:10 +0300

    change tests/test_engine.py

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,40 +1,23 @@
-import json
-from gendiff import engine
+from gendiff.engine import diff
 
 
-test_case1 = json.load(open("tests/fixtures/file1.json"))
-test_case2 = json.load(open("tests/fixtures/file2.json"))
-result1 = ['{', '  - follow: false', '    host: hexlet.io',
-        '  - proxy: 123.234.53.22', '  - timeout: 50',
-        '  + timeout: 20', '  + verbose: true', '}']
-def test_converting():
-        assert engine.converting(test_case1, test_case2) == result1
-
-
-test_case3 = [('two', False), ('four', 'Yes'), ('one', 1)]
-test_case4 = [('one', 1), ('two', True), ('tree', 'no')]
-test_case5 = [('one', 1), ('two', True), ('tree', 'no'),
-        ('two', False), ('four', 'Yes')]
-result = ['{', '  + four: Yes', '    one: 1', '  - tree: no',
-                '  - two: true', '  + two: false', '}']
-def test_create_new_list():
-        assert engine.create_new_list(test_case5, test_case4, test_case3) == result
-
-
-test_case6 = ('fllow', 5)
-test_case7 = (124, False)
-def test_covert_to_string():
-        assert engine.convert_to_string(test_case6, '-') == '  - fllow: 5'
-        assert engine.convert_to_string(test_case7, ' ') == '    124: false'
-
-
-test_case8 = ['one', 'two', 'tree']
-test_case9 = ['two', 'four']
-test_case10 = []
-result2 = ['one', 'two', 'tree', 'four']
-def test_unite():
-    assert engine.unite(test_case8, test_case9) == result2
-    assert engine.unite(test_case10, test_case9) == ['two', 'four']
-    assert engine.unite(test_case10, test_case10) == []
-
-
+test_case1 = {'file': 1}
+test_case2 = {}
+test_case3 = {'file': 1, 'doc': False}
+test_case4 = {'file': 5, 'doc': False}
+test_case5 = {'file': None, 'doc': {'hello': 'world'}}
+test_case6 = {'file': None, 'doc': {'hello': 'world!', 'good': 'bad'}} 
+def test_diff():
+    assert diff.diff(test_case1, test_case2) == \
+            [{'name': 'file', 'status': 'available', 'value': 1}]
+    assert diff.diff(test_case1, test_case3) == \
+            [{'name': 'doc', 'status': 'added', 'value': False},
+             {'name': 'file', 'status': 'same', 'value': 1}]
+    assert diff.diff(test_case3, test_case4) == \
+            [{'name': 'doc', 'status': 'same', 'value': False},
+             {'name': 'file', 'status': 'changed', 'value before': 1, 'value after': 5}]
+    assert diff.diff(test_case5, test_case6) == \
+            [{'name': 'doc', 'status': 'parent', 'children': [
+             {'name': 'good', 'status': 'added', 'value': 'bad'}, {'name': 'hello',
+              'status': 'changed', 'value before': 'world', 'value after': 'world!'}]},
+             {'name': 'file', 'status': 'same', 'value': None}]
