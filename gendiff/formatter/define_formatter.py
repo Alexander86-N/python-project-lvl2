7e51707db8e5aef1491cd19fbfd02f2e1db18507
Aleksandr <nastyavin86@br.ru>
2022-04-09 21:44:41 +0300

    modified gendiff/formatter/define_formatter.py

--- gendiff/formatter/define_formatter.py
+++ gendiff/formatter/define_formatter.py
@@ -1,6 +1,9 @@
 from gendiff.formatter.stylish import stylish
+from gendiff.formatter.plain import plain
 
 
 def define_formatter(diff_list, view='stylish'):
     if view == 'stylish':
         return stylish(diff_list)
+    if view == 'plain':
+        return plain(diff_list)
