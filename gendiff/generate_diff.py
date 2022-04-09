b2c58039c2f6e1298c99984ca4ef6a610bd4566b
Aleksandr <nastyavin86@br.ru>
2022-04-09 21:45:13 +0300

    modified gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -4,12 +4,12 @@ from gendiff.engine import diff
 from gendiff.formatter.define_formatter import define_formatter
 
 
-def generate_diff(file_path1, file_path2, view='stylish'):
+def generate_diff(file_path1, file_path2, formate_name='stylish'):
     """ Collects and outputs diff. """
     file1 = determine_file_format(file_path1)
     file2 = determine_file_format(file_path2)
     result = diff.diff(file1, file2)
-    return define_formatter(result, view)
+    return define_formatter(result, formate_name)
 
 
 def determine_file_format(file_path):
