59e1197399b7d060f66e38565b226dfa88395c62
Aleksandr <nastyavin86@br.ru>
2022-04-10 21:34:39 +0300

    deleted gendiff/engine/view_change.py

--- gendiff/engine/view_change.py
+++
@@ -1,8 +0,0 @@
-def view_change(value):
-    """ Outputs values in the specified form. """
-    if isinstance(value, bool):
-        return str(value).lower()
-    if isinstance(value, type(None)):
-        return 'null'
-    else:
-        return value
