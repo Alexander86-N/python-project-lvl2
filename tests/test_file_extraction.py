4aed48647b48b2fee6030093e94d757d2e849bd4
Aleksandr <nastyavin86@br.ru>
2022-04-10 15:28:15 +0300

    modified tests/test_file_extraction.py

--- tests/test_file_extraction.py
+++ tests/test_file_extraction.py
@@ -7,6 +7,7 @@ test_file1 = "tests/fixtures/file1.json"
 test_file2 = "tests/fixtures/file1.yaml"
 test_file3 = "tests/fixtures/file2.yml"
 
+
 def test_determine_file_format():
     assert determine_file_format(test_file1) == json.load(open(test_file1))
     assert determine_file_format(test_file2) == yaml.safe_load(open(test_file2))
