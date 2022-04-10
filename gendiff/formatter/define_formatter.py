5d1ae7667088fb445743fa92dcd0cd790ead59e7
Aleksandr <nastyavin86@br.ru>
2022-04-10 12:22:10 +0300

    modified gendiff/formatter/define_formatter.py

--- gendiff/formatter/define_formatter.py
+++ gendiff/formatter/define_formatter.py
@@ -1,5 +1,6 @@
 from gendiff.formatter.stylish import stylish
 from gendiff.formatter.plain import plain
+from gendiff.formatter.json import format_json
 
 
 def define_formatter(diff_list, view='stylish'):
@@ -7,3 +8,5 @@ def define_formatter(diff_list, view='stylish'):
         return stylish(diff_list)
     if view == 'plain':
         return plain(diff_list)
+    if view == 'json':
+        return format_json(diff_list)
