1e4c1cb09d53c364072105d59ef3fcdc85483e47
Aleksandr <nastyavin86@br.ru>
2022-04-10 14:44:16 +0300

    modified gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,7 +1,6 @@
-import json
-import yaml
 from gendiff.engine import diff
 from gendiff.formatter.define_formatter import define_formatter
+from gendiff.engine.file_extraction import determine_file_format
 
 
 def generate_diff(file_path1, file_path2, formate_name='stylish'):
@@ -10,11 +9,3 @@ def generate_diff(file_path1, file_path2, formate_name='stylish'):
     file2 = determine_file_format(file_path2)
     result = diff.diff(file1, file2)
     return define_formatter(result, formate_name)
-
-
-def determine_file_format(file_path):
-    """ Determines the file format and opens it. """
-    if file_path.endswith('.json'):
-        return json.load(open(file_path))
-    if file_path.endswith('.yml') or file_path.endswith('.yaml'):
-        return yaml.safe_load(open(file_path))
