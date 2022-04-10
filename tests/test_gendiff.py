20fe6faf1972a9923d2c9281dda8b05ded43be5e
Aleksandr <nastyavin86@br.ru>
2022-04-10 14:44:40 +0300

    modified tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,3 @@
-import json
-import yaml
 from gendiff import generate_diff
 
 
@@ -21,20 +19,14 @@ with open('tests/fixtures/result_json.txt') as text:
     result4 = text.read()
 
 def test_generate_diff():
-    assert generate_diff.generate_diff(test_file1, test_file2) == result1[:-1]
-    assert generate_diff.generate_diff(test_file3, test_file4) == result1[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file6) == result2[:-1]
-    assert generate_diff.generate_diff(test_file7, test_file8) == result2[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file8) == result2[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file6, 'plain') == result3[:-1]
-    assert generate_diff.generate_diff(test_file7, test_file8, 'plain') == result3[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file8, 'plain') == result3[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file6, 'json') == result4[:-1]
-    assert generate_diff.generate_diff(test_file7, test_file8, 'json') == result4[:-1]
-    assert generate_diff.generate_diff(test_file5, test_file8, 'json') == result4[:-1]
-
-
-def test_determine_file_format():
-    assert generate_diff.determine_file_format(test_file1) == json.load(open(test_file1))
-    assert generate_diff.determine_file_format(test_file3) == yaml.safe_load(open(test_file3))
-    assert generate_diff.determine_file_format(test_file4) == yaml.safe_load(open(test_file4))
+    assert generate_diff(test_file1, test_file2) == result1[:-1]
+    assert generate_diff(test_file3, test_file4) == result1[:-1]
+    assert generate_diff(test_file5, test_file6) == result2[:-1]
+    assert generate_diff(test_file7, test_file8) == result2[:-1]
+    assert generate_diff(test_file5, test_file8) == result2[:-1]
+    assert generate_diff(test_file5, test_file6, 'plain') == result3[:-1]
+    assert generate_diff(test_file7, test_file8, 'plain') == result3[:-1]
+    assert generate_diff(test_file5, test_file8, 'plain') == result3[:-1]
+    assert generate_diff(test_file5, test_file6, 'json') == result4[:-1]
+    assert generate_diff(test_file7, test_file8, 'json') == result4[:-1]
+    assert generate_diff(test_file5, test_file8, 'json') == result4[:-1]
