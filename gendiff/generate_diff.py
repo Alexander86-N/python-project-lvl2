3c60398987e2498d850a7fde6853a615a2cab630
Aleksandr <nastyavin86@br.ru>
2022-04-12 20:30:52 +0300

    modified gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,11 +1,11 @@
-from gendiff.engine import diff
+from gendiff.diff import diff
 from gendiff.formatter.define_formatter import define_formatter
-from gendiff.engine.file_extraction import determine_file_format
+from gendiff.file_extraction import parser_file
 
 
 def generate_diff(file_path1, file_path2, formate_name='stylish'):
     """ Collects and outputs diff. """
-    file1 = determine_file_format(file_path1)
-    file2 = determine_file_format(file_path2)
-    result = diff.diff(file1, file2)
+    file1 = parser_file(file_path1)
+    file2 = parser_file(file_path2)
+    result = diff(file1, file2)
     return define_formatter(result, formate_name)
