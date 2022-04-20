0228a3fb9faae19ef4f28c5a52264fc4ed90ae14
Aleksandr <nastyavin86@br.ru>
2022-04-20 20:22:35 +0300

    modified gendiff/parsing.py

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -4,6 +4,6 @@ import yaml
 
 def parser(data, extension):
     if extension == 'json':
-        return json.load(data)
+        return json.loads(data)
     if extension == 'yaml' or extension == 'yml':
         return yaml.safe_load(data)
