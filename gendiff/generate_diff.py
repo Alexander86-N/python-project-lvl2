91257365bd9c2c96d1826493ad02c77854dae9a6
Aleksandr <nastyavin86@br.ru>
2022-04-20 20:22:05 +0300

    modified gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,15 +1,26 @@
+from pathlib import Path
 from gendiff.diff import diff
-from gendiff.data_extraction import fetches_data
-from gendiff.formatter import stylish
-from gendiff.formatter import plain
-from gendiff.formatter import json
+from gendiff.parsing import parser
+from gendiff.formatter.format import format
 
 
 def generate_diff(file_path1, file_path2, formate_name='stylish'):
     """ Collects and outputs diff. """
-    data1 = fetches_data(file_path1)
-    data2 = fetches_data(file_path2)
+    data1 = parser(read_file(file_path1), get_format(file_path1))
+    data2 = parser(read_file(file_path2), get_format(file_path2))
     result = diff(data1, data2)
-    return {'stylish': stylish.format(result),
-            'plain': plain.format(result),
-            'json': json.format(result)}.get(formate_name)
+    return format(result, formate_name)
+
+
+def read_file(filename):
+    result = ''
+    with open(filename) as file_data:
+        result = file_data.read()
+    return result
+
+
+def get_format(filename):
+    """ Specifies the format of this file. """
+    path = Path(filename)
+    extension = path.suffix
+    return extension[1:]
